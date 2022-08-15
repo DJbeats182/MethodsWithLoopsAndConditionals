0dbad98cb69cb24f83dd3e7446007d7cc3aed265
DJ Bates <blooviper@gmail.com>
2022-08-15 13:23:41 -0600

    Made some more changes

--- MethodsWithLoopsAndConditionals/Methods.cs
+++ MethodsWithLoopsAndConditionals/Methods.cs
@@ -4,108 +4,136 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MethodsWithLoopsAndConditionals
-{
-    internal class Methods
-    {
-        public static void PrintRange(int ceiling, int floor)
-        {              
-            for (int i = ceiling; i >= floor; i--)
-            {
-                if (ceiling < floor)
-                {
-                    Console.WriteLine("Make sure you entered the larger number first");
-                }
-                else
-                {
-                    Console.WriteLine(i);
-                }                
-            }
-        }
 
-        public static void RangeByThree(int a, int b)
+namespace MethodsWithLoopsAndConditionals;
+
+internal class Methods
+{
+    public static void PrintRange(int ceiling, int floor)
+    {
+        for (int i = ceiling; i >= floor; i--)
         {
-            for (int i = a; i <= b; i+=3)
+            if (ceiling < floor)
+            {
+                Console.WriteLine("Make sure you entered the larger number first");
+            }
+            else
             {
                 Console.WriteLine(i);
             }
         }
+    }
 
-        public static void EqualTo(int num1, int num2)
+    public static void RangeByThree(int a, int b)
+    {
+        for (int i = a; i <= b; i += 3)
         {
+            Console.WriteLine(i);
+        }
+    }
 
-            if (num1 == num2)
+    public static void EqualTo(int num1, int num2)
+    {
+
+        if (num1 == num2)
+        {
+            Console.WriteLine($"{num1} is equal to {num2}");
+        }
+        else
+        {
+            Console.WriteLine($"{num1} is NOT equal to {num2}");
+        }
+
+    }
+
+    public static void EvenOrOdd(int response)
+    {
+        if (response % 2 == 0)
+        {
+            Console.WriteLine($"{response} is an EVEN number");
+        }
+        else
+        {
+            Console.WriteLine($"{response} is an ODD number");
+        }
+    }
+
+    public static void PosOrNeg(int num)
+    {
+        if (num > 0)
+        {
+            Console.WriteLine($"{num} is a positive number");
+        }
+        else if (num == 0)
+        {
+            Console.WriteLine($"{num} is neither positive or negative");
+        }
+        else
+        {
+            Console.WriteLine($"{num} is a negative number");
+        }
+    }
+
+    public static void VotingAge()
+    {
+        Console.WriteLine("Hello");
+        bool candidateAge;
+        do
+        {
+            //Console.WriteLine("Enter in your age:\n");
+            candidateAge = int.TryParse(Console.ReadLine(), out int input);
+            if (candidateAge)
             {
-                Console.WriteLine($"{num1} is equal to {num2}");
+                if (input >= 18)
+                {
+                    Console.WriteLine("Awesome, it looks like you are old enough to Vote!!!");
+                }
+                else
+                {
+                    int yearsToWait = 18 - input;
+                    Console.WriteLine($"Sorry you are not old enough to vote, wait {yearsToWait}" +
+                        $" more years and then youll be old enough to vote.");
+                }
             }
             else
             {
-                Console.WriteLine($"{num1} is not equal to {num2}");
+                Console.WriteLine("Sorry incorrect input. Please try again!");                
             }
+        } while (!candidateAge);
+        
+        
+    }
 
-        }
-
-        public static void EvenOrOdd(int response)
+    public static void RangeCheck(int numCheck)
+    {
+        if (numCheck >= -10 && numCheck <= 10)
         {
-            if(response % 2 == 0)
-            {
-                Console.WriteLine($"{response} is an even number");
-            }
-            else
-            {
-                Console.WriteLine($"{response} is an odd number");
-            }
+            Console.WriteLine($"it looks like {numCheck} does fit with the range -10 to 10!");
         }
-
-        public static void PosOrNeg(int num)
+        else
         {
-            if(num > 0)
-            {
-                Console.WriteLine($"{num} is a positive number");
-            }
-            else
-            {
-                Console.WriteLine($"{num} is a negative number");
-            }
+            Console.WriteLine($"Sorry! {numCheck} falls outside of the range -10 to 10.");
         }
+    }
 
-        public static void VotingAge(int age)
+    public static void MultTable(int multiply)
+    {
+        for (int i = 1; i <= 12; i++)
         {
-            if (age >= 18)
-            {
-                Console.WriteLine("Awesome, it looks like you are old enough to Vote!!!");
-            }
-            else
-            {
-                int yearsToWait = 18 - age; 
-                Console.WriteLine($"Sorry you are not old enough to vote, wait {yearsToWait}" +
-                    $" more years and then youll be old enough to vote.");
-            }
+            int answer = i * multiply;
+            Console.WriteLine($"{i} X {multiply} = {answer}");
         }
+    }
 
-        public static void RangeCheck(int numCheck)
-        {
-            if(numCheck >= -10 && numCheck <= 10)
-            {
-                Console.WriteLine($"it looks like {numCheck} does fit with the range -10 to 10!");
-            }
-            else
-            {
-                Console.WriteLine($"Sorry! {numCheck} falls outside of the range -10 to 10.");
-            }
-        }
-
-        public static void MultTable(int multiply)
-        {
-            for(int i = 1; i <= 12; i++)
-            {
-                int answer = i * multiply;
-                Console.WriteLine($"{i} X {multiply} = {answer}");
-            }
-        }
-           
-
-
-
+    public static void delay(int Time_delay) // I did not right this method haha, I'm just using it to make my program run a little smoother
+    {
+        int i = 0;
+        var ameTir = new System.Timers.Timer();
+        var delayTimer = new System.Timers.Timer();
+        delayTimer.Interval = Time_delay;
+        delayTimer.AutoReset = false; //so that it only calls the method once
+        delayTimer.Elapsed += (s, args) => i = 1;
+        delayTimer.Start();
+        while (i == 0) { };
     }
 }
--- MethodsWithLoopsAndConditionals/Program.cs
+++ MethodsWithLoopsAndConditionals/Program.cs
@@ -4,82 +4,82 @@
     {
         static void Main(string[] args)
         {
+            //Console.WriteLine(Methods.PrintRange(34, 5);
+            //Console.WriteLine("Welcome to my Methods with Loops and Conditionals Program!!\n");
+            //Methods.delay(2000);
+            //Console.WriteLine("Press any \"Enter\" to continue");
+            //Console.ReadLine();
 
-            Console.WriteLine("Welcome to my Methods with Loops and Conditionals Program!!");
-            Console.WriteLine("Press any key to continue");
-            Console.ReadLine();
 
+            ////------------User-Input-Range-----------------------------
+            //Console.WriteLine("Lets start by making a list of numbers in decsending order.\n");
+            //Console.WriteLine("Enter the largest number in your range:\n");
+            //int ceiling = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter the smallest number in your range:\n");
+            //int floor = int.Parse(Console.ReadLine());
+            //Methods.PrintRange(ceiling, floor);
+            //Console.WriteLine("There is your list :)\n");
+            //Console.WriteLine("Press \"Enter\" to continue");
+            //Console.ReadLine();
 
-            //------------User-Input-Range-----------------------------
-            Console.WriteLine("Lets start by making a list of numbers in decsending order.");
-            Console.WriteLine("Enter the largest number in your range:");
-            int ceiling = int.Parse(Console.ReadLine());
-            Console.WriteLine("Enter the smallest number in your range:");
-            int floor = int.Parse(Console.ReadLine());
-            Methods.PrintRange(ceiling, floor);
-            Console.WriteLine("There is your list :)");
-            Console.WriteLine("Press any key to continue");
-            Console.ReadLine();
+            ////------------Range-By-Three----------------------------------
+            //Console.WriteLine("Next let's make a list of numbers in ascending order and increment by 3.\n");
+            //Console.WriteLine("Enter the smallest number in your range:\n");
+            //int a = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter the largest number in your range:\n");
+            //int b = int.Parse(Console.ReadLine());
+            //Methods.RangeByThree(a, b);
+            //Console.WriteLine("There is your list :)\n");
+            //Console.WriteLine("Press \"Enter\" to continue");
+            //Console.ReadLine();
 
-            //------------Range-By-Three----------------------------------
-            Console.WriteLine("Next let's make a list of numbers in ascending order and increment by 3.");
-            Console.WriteLine("Enter the smallest number in your range:");
-            int a = int.Parse(Console.ReadLine());
-            Console.WriteLine("Enter the largest number in your range:");
-            int b = int.Parse(Console.ReadLine());
-            Methods.RangeByThree(a, b);
-            Console.WriteLine("There is your list :)");
-            Console.WriteLine("Press any key to continue");
-            Console.ReadLine();
+            ////------------Equality-Test----------------------------------
+            //Console.WriteLine("Now we will test to the equality of two numbers.\n");
+            //Console.WriteLine("Let's see if the numbers are equal to each other.\n");
+            //Console.WriteLine("Give me a number\n");
+            //int firstNum = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Give me another number\n");
+            //int secondNum = int.Parse(Console.ReadLine());
+            //Methods.EqualTo(firstNum, secondNum);
+            //Console.WriteLine("Press \"Enter\" to continue");
+            //Console.ReadLine();
 
-            //------------Equality-Test----------------------------------
-            Console.WriteLine("Now we will test to the equality of two numbers.");
-            Console.WriteLine("Let's see if the numbers are equal to each other.");
-            Console.WriteLine("Give me a number");
-            int firstNum = int.Parse(Console.ReadLine());
-            Console.WriteLine("Give me another number");
-            int secondNum = int.Parse(Console.ReadLine());
-            Methods.EqualTo(firstNum, secondNum);
-            Console.WriteLine("Press any key to continue");
-            Console.ReadLine();
+            ////------------Even-Or-Odd--------------------------------------
+            //Console.WriteLine("Okay time to see whether your number is even or odd.\n");
+            //Console.WriteLine("Enter your number:");
+            //int testNum = int.Parse(Console.ReadLine());
+            //Methods.EvenOrOdd(testNum);
+            //Console.WriteLine("Press \"Enter\" to continue\n");
+            //Console.ReadLine();
 
-            //------------Even-Or-Odd--------------------------------------
-            Console.WriteLine("Okay time to see whether your number is even or odd.");
-            Console.WriteLine("Enter your number:");
-            int testNum = int.Parse(Console.ReadLine());
-            Methods.EvenOrOdd(testNum);
-            Console.WriteLine("Press any key to continue");
-            Console.ReadLine();
-
-            //------------Positive-Or-Negative--------------------------------
-            Console.WriteLine("Similarily let's see if a number is positve or negative.");
-            //Console.WriteLine("Now let's see whether a number is positive or negative.");
-            Console.WriteLine("What is the number you would like to test?:");
-            int checkNumSign = int.Parse(Console.ReadLine());
-            Methods.PosOrNeg(checkNumSign);
-            Console.WriteLine("Press any key to continue");
-            Console.ReadLine();
+            ////------------Positive-Or-Negative--------------------------------
+            //Console.WriteLine("Similarily let's see if a number is positve or negative.\n");
+            ////Console.WriteLine("Now let's see whether a number is positive or negative.");
+            //Console.WriteLine("What is the number you would like to test?:\n");
+            //int checkNumSign = int.Parse(Console.ReadLine());
+            //Methods.PosOrNeg(checkNumSign);
+            //Console.WriteLine("Press \"Enter\" to continue\n");
+            //Console.ReadLine();
 
             //------------Voting-Age-Test---------------------------------------------------
-            Console.WriteLine("Are you old enough to Vote?");
-            Console.WriteLine("Enter in your age:");
-            int candidateAge = int.Parse(Console.ReadLine());
-            Methods.VotingAge(candidateAge);
-            Console.WriteLine("Press any key to continue");
+            Console.WriteLine("Are you old enough to Vote?\n");           
+            Methods.VotingAge();
+            Console.WriteLine("Press \"Enter\" to continue\n");
             Console.ReadLine();
 
             //-----------Range-Check------------------------------------------------------------
-            Console.WriteLine("Is your number within the range: -10 to 10?:");
+            Console.WriteLine("Is your number within the range: -10 to 10?:\n");
             int check = int.Parse(Console.ReadLine());
             Methods.RangeCheck(check);
-            Console.WriteLine("Press any key to continue");
+            Console.WriteLine("Press \"Enter\" to continue\n");
             Console.ReadLine();
 
             //---------------Multiplication-Table-----------------------------------------------
-            Console.WriteLine("Input a number between 1 and 12 to recieve its multiplication table:");
+            Console.WriteLine("Input a number between 1 and 12 to recieve its multiplication table:\n");
             int multTab = int.Parse(Console.ReadLine());
             Methods.MultTable(multTab);
             Console.WriteLine("You are finished with the program!");
+            Console.WriteLine("Press \"Enter\" to Exit");
             Console.ReadLine();
 
         }
